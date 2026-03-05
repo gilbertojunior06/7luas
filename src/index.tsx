deb61b0939af6886968883f5f82ebfc9fa396c47
gilberto junior <gilberto silverio>
2026-03-05 20:35:01 -0300

    Atualizei App.jsx e App.css

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Olá, Gilberto 🚀</h1>
+      <p>Seu React + Vite está rodando!</p>
+    </div>
+  )
+}
+
+export default App
