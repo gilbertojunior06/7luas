54cebfb77c6e72f61e456ce6a5acbe990614dcd4
gilberto junior <gilberto silverio>
2026-03-09 19:28:53 -0300

    Atualizações dos componentes e estilos

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,13 +3,13 @@ import "./style/Footer.css";
 export default function Footer() {
   return (
     <div className="footer">
-      <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+      <a href="https://www.facebook.com/sala7luas/" target="_blank" rel="noopener noreferrer">
         <img src="/7luas/facebook.png" alt="Facebook" className="social-icon" />
       </a>
-      <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+      <a href="https://www.instagram.com/sala7luas/" target="_blank" rel="noopener noreferrer">
         <img src="/7luas/instagram.png" alt="Instagram" className="social-icon" />
       </a>
-      <a href="https://wa.me/5511999999999" target="_blank" rel="noopener noreferrer">
+      <a href="https://wa.me/5511994994145" target="_blank" rel="noopener noreferrer">
         <img src="/7luas/whatsapp.png" alt="WhatsApp" className="social-icon" />
       </a>
     </div>
